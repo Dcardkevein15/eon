6c02c67856223ba2215482f744ce5df0b3cd34ef
Oscar Ropero <dcardkevein10@gmail.com>
2025-10-03 03:50:10 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "YOUR_API_KEY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "YOUR_AUTH_DOMAIN",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "YOUR_PROJECT_ID",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "YOUR_STORAGE_BUCKET",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "YOUR_MESSAGING_SENDER_ID",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "YOUR_APP_ID",
+  apiKey: "AIzaSyChzwHWlQOFkj_aU8j1KwFNw1UqQm6W9F0",
+  authDomain: "studio-3422235219-dd152.firebaseapp.com",
+  projectId: "studio-3422235219-dd152",
+  storageBucket: "studio-3422235219-dd152.appspot.com",
+  messagingSenderId: "535414415577",
+  appId: "1:535414415577:web:eef956eb5c4c063892dec8",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
