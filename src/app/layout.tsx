69741c3cd75ea67b9d28c770c880fc25cbb54511
Oscar Ropero <dcardkevein10@gmail.com>
2025-10-12 22:50:56 +0000

    Pero yo quiero que el primer color que cargue la primera vez que se ingr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className={cn(inter.variable, 'font-body antialiased')}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light"
           enableSystem
           disableTransitionOnChange
         >
