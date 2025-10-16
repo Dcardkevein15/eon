5d3decd1c4009c44e7fcdefbdc2451da7523f6fd
Oscar Ropero <dcardkevein10@gmail.com>
2025-10-16 04:40:24 +0000

    Me acabo de dar cuenta que cuando ingreso por primera vez con el enlace

--- src/components/chat/empty-chat.tsx
+++ src/components/chat/empty-chat.tsx
@@ -319,6 +319,8 @@ export default function EmptyChat({ createChat }: EmptyChatProps) {
             isLoading={isCreatingChat}
             suggestions={[]}
             onClearSuggestions={() => {}}
+            onRefreshSuggestions={() => {}}
+            isRefreshingSuggestions={false}
           />
         </div>
       </div>
