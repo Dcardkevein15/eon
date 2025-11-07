ec4ed8033fbbac17f76dac66e125d029927f78cd
Oscar Ropero <dcardkevein10@gmail.com>
2025-11-07 16:19:27 +0000

    como dice que eres un pendejo yo mismo he restaurado la aplicación para

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: "AIzaSyDzPL70mB-pb7SHXY-5RjrlCrvysRwYc5g",
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
